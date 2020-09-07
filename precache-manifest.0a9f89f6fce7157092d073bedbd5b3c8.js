8059ea07208eea02447c7e5e6d77381f44df0c86
Adam <57396704+Sankti@users.noreply.github.com>
2020-09-07 17:05:18 +0200

    Updates

---
+++ precache-manifest.0a9f89f6fce7157092d073bedbd5b3c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be2968d9f8d353893d7c7dea61dae705",
+    "url": "/Users_Rolodex/index.html"
+  },
+  {
+    "revision": "a33e6089af71f0beb56a",
+    "url": "/Users_Rolodex/static/css/main.da36799c.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/Users_Rolodex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Users_Rolodex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a33e6089af71f0beb56a",
+    "url": "/Users_Rolodex/static/js/main.5de723b6.chunk.js"
+  },
+  {
+    "revision": "aabd516b1ea58ea465df",
+    "url": "/Users_Rolodex/static/js/runtime-main.b35ef58b.js"
+  }
+]);
